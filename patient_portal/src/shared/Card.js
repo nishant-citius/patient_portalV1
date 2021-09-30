bbab74b35120f584906b694992c22fdcce1fafa7
Ajinkya Mohanrao Pathak <AjinkyaP3@citiustech.com>
2021-09-30 16:35:07 +0530

    Added Services & Shared folders.

---
+++ patient_portal/src/shared/Card.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class Card extends React.Component {
+  render() {
+    return (
+      <div className="card shadow-lg p-3 mb-5 bg-white rounded text-center">
+        <div className="card-header text-success">
+          <h6>{this.props.title}</h6>
+        </div>
+
+        <div className="card-body">
+          {this.props.children}
+          {this.props.footer}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Card;
