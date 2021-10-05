af9c2514c8a0c268f4693a18c05a1e3ece1bed52
Nishant Chaubey <NishantC3@citiustech.com>
2021-10-05 17:53:22 +0530

    Login page changes

--- patient_portal/src/components/Login.js
+++ patient_portal/src/components/Login.js
@@ -1,4 +1,5 @@
 import { React, useState } from "react";
+import axios from "axios";
 
 const Login = () => {
   const tempUser = {
@@ -6,6 +7,38 @@ const Login = () => {
     password: "",
   };
   const [user, setUser] = useState(tempUser);
+
+  const handleUserChange = (e) => {
+    const name = e.target.name,
+      value = e.target.value;
+    setUser({ ...user, [name]: value });
+  };
+
+  const submitUserData = (e) => {
+    e.preventDefault();
+    let newUserData = { ...user };
+    // props.addUserHandler(newUserData);
+    // setUser(tempUser);
+
+    onSubmit(newUserData);
+  };
+
+  const checkEmail = (serverUsers, formData) => {
+    const user = serverUsers.find((user) => ((user.email === formData.username)&&(user.password ==formData.password)) ); // extract the email from the formData
+    if (user) return user;
+  };
+
+  const onSubmit = async (formData) => {
+    const user = await axios
+      .get("http://localhost:9999/users")
+      .then((res) => checkEmail(res.data, formData));
+    if (user) {
+      console.log("User loged in")
+      // do whatever you want here with the existence user store.
+    } else {
+      alert("email doesnot exit");
+    }
+  };
 
   return (
     <>
@@ -16,7 +49,7 @@ const Login = () => {
           <form className="login-form">
             <div className="form-group">
               <label htmlFor="user name">User Name</label>
-              <input type="text" className="form-control" name="username" />
+              <input type="text" className="form-control" name="username" onChange={handleUserChange} />
               <small id="emailHelp" className="form-text text-muted">
                 Forgot username?
               </small>
@@ -27,7 +60,8 @@ const Login = () => {
               <input
                 type="password"
                 className="form-control"
-                name="user name"
+                name="password"
+                onChange={handleUserChange}
               />
               <small id="passwordHelp" className="form-text text-muted">
                 Forgot password?
@@ -35,13 +69,13 @@ const Login = () => {
             </div>
             <br />
             <div>
-              <label class="form-check-label" htmlFor="Check1">
+              <label className="form-check-label" htmlFor="Check1">
                 Remember me
               </label>
               <input type="checkbox" className="form-check-input" id="Check1" />
             </div>
             <br />
-            <button type="submit" className="btn btn-primary">
+            <button type="submit" className="btn btn-primary" onClick={submitUserData}>
               Login
             </button>
           </form>
