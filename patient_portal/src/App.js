b41f17f708a363cb364f47c6dbf900ea0c008750
Susmitha <susmithachinthakindi1281@outlook.com>
2021-10-07 15:39:36 +0530

    Image slider was added

--- patient_portal/src/App.js
+++ patient_portal/src/App.js
@@ -1,15 +1,19 @@
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
+import {Link} from "react-router-dom";
 import PatientPortalHome from "./components/PatientPortalHome";
 import Login from "./components/Login";
 import RegisterUser from "./components/RegisterUser";
 import Admin_dashboard from "./components/admin/Admin_dashboard";
 import Patient_dashboard from "./components/patient/Patient_dashboard";
 import Physician_dashboard from "./components/physician/Physician_dashboard";
+import Slider from "./components/Slider";
+
 
 function App() {
   return (
     <>
+      <div>
       <Switch>
         <Route exact path="/" component={PatientPortalHome} />
         <Route path="/login" component={Login} />
@@ -18,6 +22,11 @@ function App() {
         <Route path="/patient" component={Patient_dashboard} />
         <Route path="/physician" component={Physician_dashboard} /> 
       </Switch>
+      </div>
+
+      <div>
+        <Slider />
+      </div>
     </>
   );
 }
---
+++ patient_portal/src/components/sliderImage.js
@@ -0,0 +1,38 @@
+import First from "../assets/First.jpg";
+import Second from "../assets/Second.jpg";
+
+export default [
+  {
+    title: "",
+    urls: First,
+  },
+  {
+    title: "",
+    urls: Second,
+  },
+  {
+    title: "",
+    
+    urls: "https://www.kailashhealthcare.com/healthpackagesnew/images/slider-1.png",
+  },
+  {
+    title: "",
+    
+    urls: "https://lh3.googleusercontent.com/proxy/yudAkIeR7IWfb4rG6-RP1PApLa6-aVEJTQGQm_o4_d41KSRn7C3rO5_rqdIVOC4tM4qWuYJqpegSQEzmdYs3DVe5MJ7GHmCUfEfGS56flLjW-nr9mhNMGBICh9IFqRGW_A",
+  },
+  {
+    title: "",
+    
+    urls: "https://www.springvalleyhospital.com/sites/springvalleyhospital.com/files/styles/art_directed_banner__desktop_1700x605/public/AC-recovery_herobanner_6_1700.jpg?itok=ZXUQXtiJ",
+  },
+  {
+    title: "",
+    
+    urls: "https://kimsthospital.in/images/abt-bnr07.jpg",
+  },
+  {
+    title: "",
+    
+    urls: "https://kims-app-server.s3.ap-south-1.amazonaws.com/images/blogs/endoscopy-in-the-treatment-of-pancreatic-diseases_1615443806.png",
+  },
+];
