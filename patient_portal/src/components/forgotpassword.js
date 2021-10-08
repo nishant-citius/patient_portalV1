c2a0084433fd3d32cdd68ab2062f8d520eca7095
Nishant Chaubey <NishantC3@citiustech.com>
2021-10-08 14:48:18 +0530

    forget password and login and Reg forms

---
+++ patient_portal/src/components/forgotpassword.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class ForgotPassword extends React.Component {
+    render() { 
+        return <div>forgot password page</div>;
+    }
+}
+ 
+export default ForgotPassword;
