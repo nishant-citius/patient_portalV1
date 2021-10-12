843ae32c65251d4f6268bfbff36391f8a6fdf760
T Shiva Prasad <TShivaP@citiustech.com>
2021-10-12 10:14:59 +0530

    phy changes

--- patient_portal/src/components/physician/Physician_dashboard.js
+++ patient_portal/src/components/physician/Physician_dashboard.js
@@ -11,14 +11,13 @@ const Physician_dashboard = () => {
 
   return (
     <div className='m-4'>
+      <div className="row ">
       <div className="col-6">
       <Calendar
         onChange={onChange}
         value={value}
         className="mb-4"
       />
-      </div>
-      <div className="col-6">
       <Dropdown>
         <Dropdown.Toggle variant="success">
         Select Appointments
@@ -35,9 +34,43 @@ const Physician_dashboard = () => {
           </Dropdown.Item>
         </Dropdown.Menu>
       </Dropdown>
-     
       </div>
-       <button className="btn btn-primary m-4">join online consultaion</button>
+      <div className="col-6 mb-4">
+      <table className="table mt-4">
+        <thead class="thead-dark">
+             <tr>
+                  <th scope="col"># </th>
+                  <th scope="col">Patient id</th>
+                  <th scope="col">Patient Name</th>
+                  <th scope="col">catagory</th>
+              </tr>
+         </thead>
+           <tbody>
+               <tr>
+               <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+      </tr>
+            </tbody>
+        </table>
+      <button className="btn btn-primary mt-4" href="#"> join online consultaion</button>
+      </div>
+      </div>
+      
+      
       </div>
   );
 };
