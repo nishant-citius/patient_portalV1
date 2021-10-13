1aa3f1c2328722f96c65b006216f14eead521811
T Shiva Prasad <TShivaP@citiustech.com>
2021-10-13 14:44:09 +0530

    login jwt

--- patient_portal/src/redux/actions/userActionCreater.js
+++ patient_portal/src/redux/actions/userActionCreater.js
@@ -43,6 +43,48 @@ export function Register(user) {
   };
 }
 
+export function Login(user)
+{
+    let payload = {
+        globalmessage: '',
+        isLoggedIn: false,
+        role: '',
+        authToken: ''
+    }
+    return (dispatch) => {
+        
+        axios.post(URLS.LOGIN_USER, JSON.stringify(user), config).then(
+            (response) => {
+                payload.globalmessage = `User with email id ${user.email} loggedin successfully`;
+                payload.isLoggedIn = true;
+                payload.role = response.data.user.role;
+                payload.authToken = response.data.accessToken;
+                dispatch({type: actions.LOGIN, payload: payload});
+            },
+            (error) => {
+                payload.globalmessage = `${error.response.data}`;
+                payload.isLoggedIn = false;
+                payload.authToken = '';
+                payload.role = '';
+                dispatch({type: actions.LOGIN, payload: payload});
+            })
+    };
+}
+
+export function Logout()
+{
+    let payload = {
+        globalmessage: 'LOGGED OUT',
+        isLoggedIn: false,
+        role: '',
+        authToken: ''
+    }
+    return (dispatch) => {
+        return dispatch({type: actions.LOGOUT, payload: payload});
+    };
+}
+
+
 export function GetAllUsersAsync() {
   return (dispatch) => {
     userService.GetAllUsers().then(
@@ -114,14 +156,14 @@ export function AddMedicationAndAllergiesAsync(user) {
   };
 }
 
-export function loginUser() {
-  return {
-    type: actions.LOGIN,
-  };
-}
+// export function loginUser() {
+//   return {
+//     type: actions.LOGIN,
+//   };
+// }
 
-export function logoutUser() {
-  return {
-    type: actions.LOGOUT,
-  };
-}
+// export function logoutUser() {
+//   return {
+//     type: actions.LOGOUT,
+//   };
+// }
