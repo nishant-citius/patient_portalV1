df22a0a51ca806278178cf170f0a8a8aac84bdc8
Ajinkya Mohanrao Pathak <AjinkyaP3@citiustech.com>
2021-10-13 16:20:10 +0530

    Added JWT Login & Logout...

--- patient_portal/src/redux/actions/userActionCreater.js
+++ patient_portal/src/redux/actions/userActionCreater.js
@@ -43,47 +43,46 @@ export function Register(user) {
   };
 }
 
-export function Login(user)
-{
-    let payload = {
-        globalmessage: '',
-        isLoggedIn: false,
-        role: '',
-        authToken: ''
-    }
-    return (dispatch) => {
-        
-        axios.post(URLS.LOGIN_USER, JSON.stringify(user), config).then(
-            (response) => {
-                payload.globalmessage = `User with email id ${user.email} loggedin successfully`;
-                payload.isLoggedIn = true;
-                payload.role = response.data.user.role;
-                payload.authToken = response.data.accessToken;
-                dispatch({type: actions.LOGIN, payload: payload});
-            },
-            (error) => {
-                payload.globalmessage = `${error.response.data}`;
-                payload.isLoggedIn = false;
-                payload.authToken = '';
-                payload.role = '';
-                dispatch({type: actions.LOGIN, payload: payload});
-            })
-    };
+export function Login(user) {
+  let payload = {
+    globalmessage: "",
+    isLoggedIn: false,
+    role: "",
+    authToken: "",
+    loggedUserInfo: {},
+  };
+  return (dispatch) => {
+    axios.post(URLS.LOGIN_USER, JSON.stringify(user), config).then(
+      (response) => {
+        payload.globalmessage = `User with email id ${user.email} loggedin successfully`;
+        payload.isLoggedIn = true;
+        payload.role = response.data.user.role;
+        payload.authToken = response.data.accessToken;
+        payload.loggedUserInfo = response.data.user;
+        dispatch({ type: actions.LOGIN, payload: payload });
+      },
+      (error) => {
+        payload.globalmessage = `${error.response.data}`;
+        payload.isLoggedIn = false;
+        payload.authToken = "";
+        payload.role = "";
+        dispatch({ type: actions.LOGIN, payload: payload });
+      }
+    );
+  };
 }
 
-export function Logout()
-{
-    let payload = {
-        globalmessage: 'LOGGED OUT',
-        isLoggedIn: false,
-        role: '',
-        authToken: ''
-    }
-    return (dispatch) => {
-        return dispatch({type: actions.LOGOUT, payload: payload});
-    };
+export function Logout() {
+  let payload = {
+    globalmessage: "LOGGED OUT",
+    isLoggedIn: false,
+    role: "",
+    authToken: "",
+  };
+  return (dispatch) => {
+    return dispatch({ type: actions.LOGOUT, payload: payload });
+  };
 }
-
 
 export function GetAllUsersAsync() {
   return (dispatch) => {
