82874fa09aa2abdb6af3382ce236cc454549271f
Supreeya <supreeya.kulkarni@outlook.com>
2021-10-25 13:24:23 +0530

    Backend getting

--- patient_portal/src/components/admin/ImmunizationDetails.js
+++ patient_portal/src/components/admin/ImmunizationDetails.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
 import {
+  BsFillTrashFill,
+  BsFillPencilFill,
+  BsPersonFill,
+  BsCheckCircleFill,
+  BsFillXCircleFill,
   BsFillArrowLeftSquareFill,
 } from "react-icons/bs";
 
@@ -14,6 +20,38 @@ const ImmunizationDetails = () => {
       <h1 className="text-primary text-center fw-bold ">
         Immunization Details
       </h1>
+      <table className="table table-bordered shadow mt-4">
+        <thead className="table-dark">
+          <tr>
+            <th scope="col">Sr.No</th>
+            <th scope="col">Name</th>
+            <th scope="col">Vaccine Name</th>
+            <th scope="col">Vaccine Brand</th>
+            <th scope="col">Dose Details</th>
+            <th scope="col">Vaccine Date</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <span className="p-2">
+                <Link>
+                  <BsPersonFill />
+                </Link>
+              </span>
+              <span className="p-2">
+                <Link>
+                  <BsFillPencilFill />
+                </Link>
+              </span>
+              <span className="p-2">
+                <BsFillTrashFill />
+              </span>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </>
   );
 };
