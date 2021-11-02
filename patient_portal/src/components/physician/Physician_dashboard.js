cca6c7698e0cb5e7e59aee24ac5de26637c5780f
Ajinkya Mohanrao Pathak <AjinkyaP3@citiustech.com>
2021-11-02 18:52:14 +0530

    Adding Changes

--- patient_portal/src/components/physician/Physician_dashboard.js
+++ patient_portal/src/components/physician/Physician_dashboard.js
@@ -69,7 +69,7 @@ const Physician_dashboard = (props) => {
     return approved;
   }
   var dateObj = new Date();
-  var month = dateObj.getUTCMonth() + 1; //months from 1-12
+  var month = dateObj.getUTCMonth() + 1;
   var day = dateObj.getUTCDate();
   var year = dateObj.getUTCFullYear();
 
@@ -82,7 +82,6 @@ const Physician_dashboard = (props) => {
     apptList.map((appt) => {
       if (appt.appointmentDate === newdate) {
         approved += 1;
-        console.log("string" + approved);
       }
     });
     return approved;
@@ -167,6 +166,7 @@ const Physician_dashboard = (props) => {
       opacity: "50%",
     },
   }));
+
   const classes = useStyles();
   return (
     <>
@@ -179,7 +179,6 @@ const Physician_dashboard = (props) => {
                 height="130"
                 image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjqdo4r_VEzgKMlfJtWPEGW-u31ILuS6VMjQ&usqp=CAU"
               />
-              {/* <h6 className={classes.h6}>Details</h6> */}
               <CardContent className={classes.textblock1}>
                 <h6 className={classes.h6} style={{ color: "white" }}>
                   Total Appointments :
