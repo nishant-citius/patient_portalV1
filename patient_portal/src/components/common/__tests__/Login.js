5f752e39460c665997f76ae3f892c368801d9756
Pooja <Pooja@citiustech.com>
2021-11-02 12:03:24 +0530

    changes

--- patient_portal/src/components/common/__tests__/Login.js
+++ patient_portal/src/components/common/__tests__/Login.js
@@ -19,13 +19,26 @@ describe("Login Component", () => {
     );
   });
 
-  test("email check", () => {
+  it("Should capture email correctly onChange and change the props accordingly", function () {
     const wrapper = mount(jsx);
-    wrapper.find("input").simulate("change", {
-      target: {
-        value: "nc@email.com",
-      },
-    });
-    expect(wrapper.state("email")).toEqual("nc@email.com");
+    const input = wrapper.find('input[type="text"]').at(2);
+    // input.simulate('change', {target: {email: 'mail@hotmail.com'}}); -- this does not work
+
+    input.instance('input[type="text"]').value = "mail@hotmail.com";
+    input.simulate("change");
+    expect(wrapper.find('input[type="text"]').at(2).props().value).toEqual(
+      "mail@hotmail.com"
+    );
+    // Alternatively, can check state
+    // expect(component.state().email).toEqual('mail@hotmail.com');
   });
+  // test("email check", () => {
+  //   const wrapper = mount(jsx);
+  //   wrapper.find('input[type="text"]').simulate("change", {
+  //     target: {
+  //       value: "nc@email.com",
+  //     },
+  //   });
+  //   expect(wrapper.state('input[type="text"]')).toEqual("nc@email.com");
+  // });
 });
