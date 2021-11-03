04122aa25f51c66f435f5fb19e7a0970c83b3ad7
Ajinkya Mohanrao Pathak <AjinkyaP3@citiustech.com>
2021-11-03 23:40:03 +0530

    Adding Changes for Patient Vitals for the Patient Module

--- patient_portal/src/components/patient/PatientVitals.js
+++ patient_portal/src/components/patient/PatientVitals.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { connect } from "react-redux";
-import * as actioncreators from "../../redux/actions/userActionCreater";
 
 export class PatientVitals extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      isAvailable: false,
+    };
+  }
+
+  componentDidMount() {
+    if (this.props.patientvitalsDetails.length > 0) {
+      this.setState({
+        isAvailable: true,
+      });
+    }
   }
 
   render() {
-    console.log("Pooja", this.props.patientvitalsDetails);
     return (
       <>
         <div className="container mt-5">
@@ -30,13 +38,27 @@ export class PatientVitals extends React.Component {
               </tr>
             </thead>
             <tbody>
-              <td>{this.props.patientvitalsDetails[0].height}</td>
-              <td>{this.props.patientvitalsDetails[0].weight}</td>
-              <td>{this.props.patientvitalsDetails[0].blood_pressure}</td>
-              <td>{this.props.patientvitalsDetails[0].temperature}</td>
-              <td>{this.props.patientvitalsDetails[0].pulse}</td>
-              <td>{this.props.patientvitalsDetails[0].respiration}</td>
-              <td>{this.props.patientvitalsDetails[0].oxigen_saturation}</td>
+              <tr>
+                {this.state.isAvailable ? (
+                  <>
+                    <td>{this.props.patientvitalsDetails[0].height}</td>
+                    <td>{this.props.patientvitalsDetails[0].weight}</td>
+                    <td>{this.props.patientvitalsDetails[0].blood_pressure}</td>
+                    <td>{this.props.patientvitalsDetails[0].temperature}</td>
+                    <td>{this.props.patientvitalsDetails[0].pulse}</td>
+                    <td>{this.props.patientvitalsDetails[0].respiration}</td>
+                    <td>
+                      {this.props.patientvitalsDetails[0].oxigen_saturation}
+                    </td>
+                  </>
+                ) : (
+                  <td>
+                    <p className="text text-center fw-bold succeess">
+                      No Patient Vitals Available...
+                    </p>
+                  </td>
+                )}
+              </tr>
             </tbody>
           </table>
         </div>
