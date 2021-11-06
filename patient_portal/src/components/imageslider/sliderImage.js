ef12a0402533314ac363f839c087dd912c102fd4
Susmitha <susmithachinthakindi1281@outlook.com>
2021-11-06 13:51:40 +0530

    slider images are changed

--- patient_portal/src/components/imageslider/sliderImage.js
+++ patient_portal/src/components/imageslider/sliderImage.js
@@ -2,12 +2,12 @@ export default [
   {
     title: "",
     urls:
-      "https://www.kailashhealthcare.com/healthpackagesnew/images/slider-1.png",
+      " https://www.maggohospital.com/wp-content/uploads/2019/10/banner1.jpg",
   },
   {
     title: "",
     urls:
-      "https://www.maggohospital.com/wp-content/uploads/2019/10/banner1.jpg",
+      "https://www.springvalleyhospital.com/sites/springvalleyhospital.com/files/styles/art_directed_banner__desktop_1700x605/public/AC-recovery_herobanner_6_1700.jpg?itok=ZXUQXtiJ",
   },
   {
     title: "",
