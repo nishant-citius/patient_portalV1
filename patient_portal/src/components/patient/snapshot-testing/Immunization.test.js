692fea0cf7e412ae8a9a73b75735240b17d3efc5
Pooja <Pooja@citiustech.com>
2021-11-08 12:46:39 +0530

    added changes

--- patient_portal/src/components/patient/snapshot-testing/Immunization.test.js
+++ patient_portal/src/components/patient/snapshot-testing/Immunization.test.js
@@ -1,12 +1,10 @@
 import renderer from "react-test-renderer";
-//import Demographics from "../../patient/Demographics";
 import Login from "../../common/Login";
 import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
 import { LoginReducer } from "redux/reducers/loginReducer";
 import Immunization from "../Immunization";
 import { PatientImmunizationReducer } from "redux/reducers/PatientImmunizationReducer";
-//import {Immunizationreducer} from "redux/reducers/Immunizationreducer";
 describe("Immunization snapshot test", () => {
   let store;
   let jsx;
@@ -15,7 +13,6 @@ describe("Immunization snapshot test", () => {
   beforeEach(() => {
     rootReducer = combineReducers({
       login: LoginReducer,
-      //immunization: Immunizationreducer,
       patientImmunization: PatientImmunizationReducer,
     });
     store = createStore(rootReducer);
