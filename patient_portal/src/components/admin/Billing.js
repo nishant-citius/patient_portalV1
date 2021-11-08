084677ef7e91886af127e52db5cc787e4fd1e3fa
Supreeya <supreeya.kulkarni@outlook.com>
2021-11-08 13:22:16 +0530

    Changes

---
+++ patient_portal/src/components/admin/Billing.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Billing extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="text-center fw-bold">Work in Progress</h1>
+      </div>
+    );
+  }
+}
